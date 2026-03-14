4af506111f8a951c76dcbc9a28080f74d3c9e19b
Daigo Hirata <hirata-daigo-yc@ynu.jp>
2026-03-14 12:05:08 +0900

    Update daily qsd process

--- qsd_process/daily_qsd_process.sh
+++ qsd_process/daily_qsd_process.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /hsm/nu/wagasci/wg_auto_process/qsd_process
+readonly SCRIPT_DIR="/hsm/nu/wagasci/wg_auto_process/qsd_process"
 
-source ~/.bashrc
-
-./synchronize_qsd.sh
-./update_qsddb.py ${T2K_RUN}
+${SCRIPT_DIR}/synchronize_qsd.sh
+${SCRIPT_DIR}/update_qsddb.py ${T2K_RUN}
